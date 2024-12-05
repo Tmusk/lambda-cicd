a52335865dd8769dc5962efca818f99f455aab9c
tmusk <tmuskcloud@gmail.com>
2024-12-05 12:48:57 +0100

    Updates for test

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    return{
+    return {
         'statusCode': 200,
         'body': json.dumps('Hello from CICD Github Actions workflow from VSCode')
     }
